3cf112c9728842ea113eb10e32b379e90d6d782a
Bobmataplagas <alex.burgos2506@gmail.com>
2026-08-30 19:57:12 -0600

    pequeños cambios en la iluminación para mayor legibilidad

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -33,7 +33,7 @@ nav a {
   border: 1px solid rgba(255, 255, 255, 0.2);
   border-radius: 10px;
   background: rgba(255, 255, 255, 0.05);
-  color: #fff;
+  color: #414141;
   text-decoration: none;
   transition: all 0.2s ease;
   font-weight: 600;
